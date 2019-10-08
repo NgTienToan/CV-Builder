5585ec92de3771c7686b2233c5efa20e2ba71a90
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2019-10-08 15:58:35 +0700

    draw menu bar
    draw menu bar

--- src/containers/jobseeker/Menu/Menu.js
+++ src/containers/jobseeker/Menu/Menu.js
@@ -4,7 +4,48 @@ import styles from './Menu.scss';
 class Menu extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className={`container-fuild ${styles.Menu}`}>
+                <nav className={`navbar ${styles.NavbarCustom}`}>
+                    <div className="container">
+                        <div className="navbar-brand" href="#">
+                            <Logo />
+                        </div>
+                        <div id="navbarNavDropdown" className="navbar-collapse">
+                            <ul className="navbar-right">
+                                <li className="nav-item">
+                                    <a className="active">Việc làm</a></li>
+                                <li className="nav-item">
+                                    <a>Tạo CV</a></li>
+                                <li className="nav-item">
+                                    <a>Khám phá</a>
+                                </li>
+                                <li className="nav-item"><a>Công ty</a></li>
+                                <li className="nav-item"><a>Tư vấn</a></li>
+                                <li className="nav-item"><a>TạoCV Profile</a></li>
+                                
+                                <li className="dropdown">
+                                    <a>
+                                        <img></img>
+                                    </a>
+                                </li>
+                                <li className="nav-notification">
+                                    <a>
+                                        <i className="fa fa-bell noti-icon"></i>
+                                    </a>
+                                </li>
+                                <li className="nav-btn">
+                                    <a>
+                                        <span className="text-white">NHÀ TUYỂN DỤNG</span><br/>
+                                        <span className="text-white normal">Đăng tuyển & Tìm hồ sơ</span>
+                                    </a>
+                                </li>
+                                
+                            </ul>
+                            
+                        </div>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
