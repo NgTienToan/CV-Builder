eaff11e18072a5a51cb4d25986245ee1b982da64
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2019-11-22 22:19:29 +0700

    NavBar
    NavBar commit

--- src/containers/jobseeker/Homepage/Homepage.js
+++ src/containers/jobseeker/Homepage/Homepage.js
@@ -4,7 +4,8 @@ import styles from './Homepage.scss';
 
 
 // import Test from './../Test/Test';
-import TopBar from './../CvMaker/TopBar'
+import TopBar from './../CvMaker/TopBar';
+import NavBar from './../CvMaker/NavBar';
 import HomePage1 from './../HomePage1/HomePage1'
 
 class Homepage extends React.Component {
@@ -12,7 +13,11 @@ class Homepage extends React.Component {
     render(){
         return(
             <React.Fragment>
-                <HomePage1 />
+                {/* <HomePage1 /> */}
+                <div className={styles.header}>
+                    <TopBar />
+                    <NavBar /> 
+                </div>
 
 
                 </React.Fragment>
