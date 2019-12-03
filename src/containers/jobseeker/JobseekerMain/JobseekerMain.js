9edcfa32910a3e0e8163e5188493247cf8ce8a7e
NgTienToan <43052549+NgTienToan@users.noreply.github.com>
2019-12-03 09:11:04 +0700

    SignUp Form
    SignUp Form

--- src/containers/jobseeker/JobseekerMain/JobseekerMain.js
+++ src/containers/jobseeker/JobseekerMain/JobseekerMain.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Homepage from '../Homepage/Homepage';
 import Login from './../../Login/Login';
+import SignUp from './../../SignUp/SignUp';
 import Menu from '../Menu/Menu';
 import SubMenu from '../subMenu/SubMenu'
 
@@ -20,6 +21,7 @@ class JobseekerMain extends React.Component {
                 <Switch>
                     <Route path='/' exact component={Homepage} />
                     <Route path='/login' exact component={Login} />
+                    <Route path='/register' exact component={SignUp} />
                 </Switch>
                 </React.Fragment>
         )
